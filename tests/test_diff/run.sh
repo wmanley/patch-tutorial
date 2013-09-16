d33e36088a0258183970dcc15d95de0a6e614f76
William Manley <william.manley@youview.com>
2013-09-16 17:26:24 +0100

    Test robustness: Ignore changes to whitespace in markdown output
    Different versions of python-markdown produce different whitespace.  This
    defeats my naive tests which just do a diff.  This commit makes diff
    ignore whitespace changes to prevent the tests breaking for no reason in
    the future.
    
    Unfortunately, even with diff flags `-B` and `-u` diff still can't cope
    with an addition of a newline in the middle of some text so I've had to
    make a change to the expected output of the `test_diff` test.  A fix for
    this would be to normalise the expected output and the markdown output for
    instance with html tidy before doing the file compare or using wdiff for a
    word-based diff independent of newlines but both approaches would require
    additional dependencies and are probably not worth it.

--- tests/test_diff/run.sh
+++ tests/test_diff/run.sh
@@ -5,6 +5,6 @@ OUTPUT="${TMP_DIR}/out.html"
 EXPECTED_OUTPUT="expected-output.html"
 
 ../../generate-patch-tutorial.py -o "$OUTPUT" --suppress-decoration *.patch
-cmp "$OUTPUT" "$EXPECTED_OUTPUT" || { echo FAIL; diff -u "$OUTPUT" "$EXPECTED_OUTPUT"; exit 1; }
+diff -wBu "$OUTPUT" "$EXPECTED_OUTPUT" || { echo FAIL; exit 1; }
 
 rm -R "$TMP_DIR"
